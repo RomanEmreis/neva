66bb513d6d63c48c774a3514a87840961f5bbc55
Roman Emreis <r.emreis@outlook.com>
2025-08-16 17:53:15 +0400

    fixed doc test

--- neva/src/app/context.rs
+++ neva/src/app/context.rs
@@ -421,7 +421,7 @@ impl Context {
     /// #[tool]
     /// async fn generate_poem(mut ctx: Context, _topic: String) -> Result<String, Error> {
     ///     let params = ElicitRequestParams::new("What is the poem mood you'd like?")
-    ///         .with_required(("mood", "string"));
+    ///         .with_required("mood", "string");
     ///     let result = ctx.elicit(params).await?;
     ///     Ok(format!("{:?}", result.content))
     /// }
