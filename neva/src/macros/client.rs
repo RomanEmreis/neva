d4d399f79315e9eae57fcc774927db28be657d8b
Roman Emreis <r.emreis@outlook.com>
2025-08-23 15:41:35 +0400

    clippy fixes

--- neva/src/macros/client.rs
+++ neva/src/macros/client.rs
@@ -9,6 +9,7 @@ inventory::collect!(ItemRegistrar);
 
 impl ItemRegistrar {
     /// Registers a tool, prompt or resource template depending on what the [`crate::macros::ItemRegistrar`] holds
+    #[allow(dead_code)]
     fn register(&self, client: &mut Client) {
         self.0(client);
     }
@@ -16,6 +17,7 @@ impl ItemRegistrar {
 
 impl Client {
     /// Registers all declared tools, prompts and resources
+    #[allow(dead_code)]
     pub(crate) fn register_methods(&mut self) {
         for registrar in inventory::iter::<ItemRegistrar> {
             registrar.register(self);
